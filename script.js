96abed0e337cd2b4bde6a222246e848c95e60a5c
manish021124 <maneeshbalami@gmail.com>
2022-07-28 18:34:35 +0545

    update

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('list').innerHTML = null;
+document.getElementById('btn').addEventListener("click", show);
+
+function show(){
+    document.getElementById('list').innerHTML = '';
+}
