590585c29f1b03e81b73762151bac240df6a6143
manish021124 <maneeshbalami@gmail.com>
2022-07-30 10:01:10 +0545

    update

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let x = window.matchMedia("(min-width: 768px)");
+let list = document.getElementById('list');
 
 myFunction(x);
 x.addListener(myFunction);
@@ -7,13 +8,24 @@ document.getElementById('btn').addEventListener("click", show);
 
 function myFunction(x){
     if(x.matches){
-        document.getElementById('list').style.visibility = "visible";
+        list.style.visibility = "visible";
     }else{
-        document.getElementById('list').style.visibility = "hidden";
+        list.style.visibility = "hidden";
     }
 }
 
 function show(){
-    document.getElementById('list').style.visibility = "visible";
+    if(list.style.visibility == 'hidden'){
+        visible();
+    }else{
+        hidden();
+    }
 }
 
+function visible(){
+    list.style.visibility = "visible";
+}
+
+function hidden(){
+    list.style.visibility = "hidden";
+}
