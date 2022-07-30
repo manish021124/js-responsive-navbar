bab1c418b7b3159fa5748d56d66f473551a3fa9d
manish021124 <maneeshbalami@gmail.com>
2022-07-30 09:54:34 +0545

    update

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-// document.getElementById('list').style.visibility = 'hidden';
+let x = window.matchMedia("(min-width: 768px)");
+
+myFunction(x);
+x.addListener(myFunction);
 document.getElementById('btn').addEventListener("click", show);
+
+
+function myFunction(x){
+    if(x.matches){
+        document.getElementById('list').style.visibility = "visible";
+    }else{
+        document.getElementById('list').style.visibility = "hidden";
+    }
+}
 
 function show(){
     document.getElementById('list').style.visibility = "visible";
 }
+
